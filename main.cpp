c244ac74e99bee8b539a99c534f50c7f15428870
Derdák Dániel <dderdak@szintezis-net.local>
2017-04-05 22:07:54 +0200

    Field entity created

--- main.cpp
+++ main.cpp
@@ -1,9 +1,18 @@
 #include <iostream>
+#include "headers/entity/field.h"
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    Field f (1, 2, 3);
+
+    cout << "field.id: " << f.getId() << endl;
+    cout << "field.x: " << f.getCoordinateX() << endl;
+    cout << "field.y: " << f.getCoordinateY() << endl;
+    cout << "field.mineSignal: " << f.getMineSignal() << endl;
+    f.countMineSignal();
+    cout << "field.mineSignal: " << f.getMineSignal() << endl;
+
     return 0;
 }
