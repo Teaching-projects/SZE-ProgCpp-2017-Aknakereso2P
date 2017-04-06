8add4e9b94bf24c6ad2d9efd4539c8f41b74561d
Derdák Dániel <dderdak@szintezis-net.local>
2017-04-06 18:17:47 +0200

    Header file created for managing constants

---
+++ src/main/constants.h
@@ -0,0 +1,16 @@
+#define CONST_GAME_NAME                     "Mineszwiper Game"
+
+#define CONST_GAME_HEADER_WIDTH             50
+
+#define CONST_LOGGER_LOG_LEVEL_ERROR        "ERROR: "
+#define CONST_LOGGER_LOG_LEVEL_WARNING      "WARN: "
+#define CONST_LOGGER_LOG_LEVEL_INFORMATION  "INFO: "
+#define CONST_LOGGER_LOG_LEVEL_DEBUG        "DEBUG: "
+
+#define CONST_MINEFIELD_ROWS                16
+#define CONST_MINEFIELD_COLUMNS             16
+#define CONST_MINEFIELD_NUM_OF_MINES        51
+
+#define CONST_PLAYER_ANONYMOUS_FIRSTNAME    "Anonymous"
+#define CONST_PLAYER_ANONYMOUS_LASTNAME     "Anonymous"
+#define CONST_PLAYER_ANONYMOUS_NICKNAME     "player"
