5aba0c7990522c936a0058eb5be070d6e3381ce5
Derdák Dániel <dderdak@szintezis-net.local>
2017-04-09 17:16:34 +0200

    Enumerations header created

---
+++ src/main/util/enums.h
@@ -0,0 +1,34 @@
+/**
+ * The Enum EnvProfile.
+ */
+enum EnvProfile
+{
+    DEV,
+    PROD
+};
+
+/**
+ * The Enum LogLevel.
+ */
+enum LogLevel
+{
+    DEBUG,
+    INFO,
+    WARN,
+    LL_ERROR
+};
+
+/**
+ * The Enum FieldPlace.
+ */
+enum FieldPlace
+{
+	FP_OVER, // field place at the above
+	FP_LEFT_OVER, // field place at the left over
+    FP_LEFT, // field place at the left
+    FP_LEFT_UNDER, // field place at the left under
+    FP_UNDER, // field place at the under
+    FP_RIGHT_UNDER, // field place at the right under
+    FP_RIGHT, // field place at the right
+    FP_RIGHT_OVER // field place a the right over
+};
