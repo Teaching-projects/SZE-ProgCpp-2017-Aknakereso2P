df4268c4a3a75c958fa64004507020bc590d2b49
Derdák Dániel <dderdak@szintezis-net.local>
2017-04-09 17:16:08 +0200

    Constants header renamed

---
+++ src/main/util/consts.h
@@ -0,0 +1,15 @@
+#define CONST_GAME_NAME                     "Mineszwiper Game"
+
+#define CONST_TAB                           "    "
+
+#define CONST_MINEFIELD_HEADER              "ABCDEFGHIJKLMNOP"
+
+#define CONST_PLAYER_ANONYMOUS_FIRSTNAME    "Anonymous"
+#define CONST_PLAYER_ANONYMOUS_LASTNAME     "Anonymous"
+#define CONST_PLAYER_ANONYMOUS_NICKNAME     "player"
+
+#define CONST_LOGGER_LOG_LEVEL_ERROR        "ERROR"
+#define CONST_LOGGER_LOG_LEVEL_WARNING      "WARN "
+#define CONST_LOGGER_LOG_LEVEL_INFORMATION  "INFO "
+#define CONST_LOGGER_LOG_LEVEL_DEBUG        "DEBUG"
+#define CONST_LOGGER_DEFAULT_DATE_FORMAT    "%Y-%m-%d.%X"
