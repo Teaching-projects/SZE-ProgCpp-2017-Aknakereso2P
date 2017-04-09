d481c801b9ab2fa1d078fc97263396d76d0d113b
Derdák Dániel <dderdak@szintezis-net.local>
2017-04-09 17:16:21 +0200

    Improved core functions

--- src/main/logic/core.h
+++ src/main/logic/core.h
@@ -1,11 +1,37 @@
-Logger logger = Logger();
+/**
+ * Prints the given message to the console.
+ *
+ * @param m the message
+ */
+void print(std::string m)
+{
+    std::cout << m;
+}
+
+/**
+ * Prints a line break to the console.
+ */
+void println()
+{
+    std::cout << std::endl;
+}
+
+/**
+ * Prints a line break with the given message to the console.
+ *
+ * @param m the message
+ */
+void println(std::string m)
+{
+    std::cout << m << std::endl;
+}
 
 /**
  * Checks if the current environment is a DEV environment.
  *
  * @return true, if the environment is in development
  */
-bool isDevEnvironment()
+bool core_isDevEnvironment()
 {
     return CONF_GAME_ENV_PROFILE == DEV;
 }
@@ -13,15 +39,35 @@ bool isDevEnvironment()
 /**
  * "Pausing" the console screen.
  */
-void pause()
+void core_pause()
 {
     getch();
 }
 
 /**
+ * Sleep thread until given milliseconds.
+ *
+ * @param ms the milliseconds
+ */
+void core_sleep(unsigned ms)
+{
+    Sleep(ms);
+}
+
+/**
+ * Sleep thread until given seconds.
+ *
+ * @param s the seconds
+ */
+void core_sleepSecs(int s)
+{
+    core_sleep(s * 1000);
+}
+
+/**
  * Clears the console screen.
  */
-void clearScr()
+void core_clearScr()
 {
     system("CLS");
 }
@@ -32,27 +78,27 @@ void clearScr()
  * @param number the number to convert
  * @return the converted number string
  */
-std::string formatNumber(int number)
+std::string core_formatNumber(int number)
 {
     return static_cast<std::ostringstream*>( &(std::ostringstream() << number) )->str();
 }
 
 /**
- * Sleep thread until given milliseconds.
+ * Checks if the given value is in the given values array.
  *
- * @param ms the milliseconds
+ * @param value the value for search
+ * @param values the array
+ * @param length the length of the array
+ * @return true, if the value is in the given values array
  */
-void sleep(unsigned ms)
+bool core_contains( int value, int values[], int length )
 {
-    Sleep(ms);
-}
-
-/**
- * Sleep thread until given seconds.
- *
- * @param s the seconds
- */
-void sleepSecs(int s)
-{
-    sleep(s * 1000);
+    for( int i = 0; i<length; i++ )
+    {
+        if( values[i] == value )
+        {
+            return true;
+        }
+    }
+    return false;
 }
